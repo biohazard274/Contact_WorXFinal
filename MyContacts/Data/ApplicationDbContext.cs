7ee747a3ba87fe6ebe848abc9ecb954945ec6d3d
Brandon McQUade <brandonmcquade@gmail.com>
2022-08-02 09:10:25 +0200

    Audit Log

--- MyContacts/Data/ApplicationDbContext.cs
+++ MyContacts/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MyContacts.Models;
+using MyContacts.Models.CustomModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using static MyContacts.Models.AuditLog;
 
 namespace MyContacts.Data
 {
@@ -13,6 +16,7 @@ namespace MyContacts.Data
             : base(options)
         {
         }
+        public DbSet<AuditLog> AuditLog { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<CountryCode> CountryCodes { get; set; }
@@ -20,7 +24,63 @@ namespace MyContacts.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Setting> Settings { get; set; }
 
+        public virtual async Task<int> SaveChangesAsync(string userId = null)
+        {
+            OnBeforeSaveChanges();
+            var result = await base.SaveChangesAsync();
+            return result;
+        }
+        private void OnBeforeSaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            var auditEntries = new List<AuditEntry>();
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is AuditLog || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged)
+                    continue;
+                var auditEntry = new AuditEntry(entry);
+                auditEntry.TableName = entry.Entity.GetType().Name;
 
+                auditEntries.Add(auditEntry);
+                foreach (var property in entry.Properties)
+                {
+                    string propertyName = property.Metadata.Name;
+                    if (property.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.KeyValues[propertyName] = property.CurrentValue;
+                        continue;
+                    }
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            auditEntry.AuditType = AuditType.Create;
+                            auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            break;
+                        case EntityState.Deleted:
+                            auditEntry.AuditType = AuditType.Delete;
+                            auditEntry.OldValues[propertyName] = property.OriginalValue;
+                            break;
+                        case EntityState.Modified:
+                            if (property.IsModified)
+                            {
+                                auditEntry.AuditType = AuditType.Update;
+                                if (property.CurrentValue != property.OriginalValue)
+                                {
+                                    auditEntry.ChangedColumns.Add(propertyName);
+                                    auditEntry.OldValues[propertyName] = property.OriginalValue;
+                                    auditEntry.NewValues[propertyName] = property.CurrentValue;
+
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+            foreach (var auditEntry in auditEntries)
+            {
+                AuditLog.Add(auditEntry.ToAudit());
+            }
+        }
 
     }
 }
---
+++ MyContacts/Migrations/20220802033935_auditAddition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyContacts.Migrations
+{
+    public partial class auditAddition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TableName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OldValues = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NewValues = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AffectedColumns = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryKey = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLog", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLog");
+        }
+    }
+}
